149566329dfa659fa8d625c38ee549e7ac674220
zulfikar1609 <ahmadizulfikar123@gmail.com>
2025-11-22 15:23:41 +0700

    upload

--- nomer_3/app/Views/pasien.php
+++ nomer_3/app/Views/pasien.php
@@ -6,11 +6,6 @@
         <section class="section">
           <div class="section-header">
             <h1>Pasien</h1>
-            <div class="section-header-breadcrumb">
-              <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-              <div class="breadcrumb-item"><a href="#">Modules</a></div>
-              <div class="breadcrumb-item">DataTables</div>
-            </div>
           </div>
 
           <div class="section-body">
